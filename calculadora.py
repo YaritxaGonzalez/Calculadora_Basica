1ebd0b72d7ce45b93ac0f8a55044ac1d1f8e5a9f
YaritxaGonzalez <ya.gonzalezs@duocuc.cl>
2025-07-03 14:40:50 -0400

    3 nuevas  funciones
    se crea funcionalidades 5.6 y7 .suma de 3 valores, potencias y salir

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 num1 = int(input("numero 1: ")) 
 num2 = int(input("numero 2: ")) 
-
+num3= int(input("numero 3: ")) 
 valor = 0
 while True:
     print("""seleccione opcion
@@ -8,6 +8,9 @@ while True:
             2- Restar
             3- Multiplicar
             4- dividir 
+            5- Suma de 3 valores
+            6- Potencia
+            7-Salir
         """)
 
     valor = int(input("Elige una opcion: ") )     
@@ -24,6 +27,15 @@ while True:
     if valor == 4:
         print("la division es",num1/num2)
         break;
+    if valor == 5:
+        print("la suma es",num1+num2+num3)
+        break
+    if valor == 6: 
+        print("La potencia es", num1**num2)
+        break
+    if valor == 7:
+        print("Hasta pronto ")
+        break
     else:
         print("Opcion incorrecta")
         break;
